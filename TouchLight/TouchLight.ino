ad36778b85576f71a1e3ff10b45f3a4b9a84fea0
Daniel Nunns <danielbnunns@gmail.com>
2019-12-04 17:25:01 +1000

    ATTiny85 port

--- TouchLight/TouchLight.ino
+++ TouchLight/TouchLight.ino
@@ -1,78 +1,80 @@
+//#include <Arduino.h> // needed for main()//
+
+//#include <ATtinySerialOut.h>
+
+//#include <avr/pgmspace.h> // needed for P/STR()/
+
 #include <CapacitiveSensor.h>
 
 /*
- * CapitiveSense Library Demo Sketch
- * Paul Badger 2008
- * Uses a high value resistor e.g. 10M between send pin and receive pin
- * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
- * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
- */
+   Uses a high value resistor e.g. 10M between send pin and receive pin.
+   Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+   Receive pin is the sensor pin - try different amounts of foil/metal on this pin.
+*/
 
- /*
-  * Modified by Daniel Nunns, 2019.
-  * WIP.
-  */
+/*
+   Modified by Daniel Nunns, 2019.
+   WIP.
+*/
 
 
-CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+#define PIN_TOUCH_SENSOR    4
+#define PIN_TOUCH_REFERENCE 3
 
-int LED = 9;
+#define PIN_LED_CONTROL     1
+
+#define TOUCH_TOLERANCE     200
+
+// 10M resistor between pins.
+CapacitiveSensor cs = CapacitiveSensor(PIN_TOUCH_REFERENCE, PIN_TOUCH_SENSOR);
 
 long avg = 0;
 
 long newValue = 0;
 
-long tolerance = 1000;
-
 bool pressed = false;
 bool pressedBuffer = false;
 
-bool flip = false;
-int brightness = 0;
+#define BRIGHTNESS_LEVELS 4
+uint8_t brightness[] = {0, 100, 200, 255};
+uint8_t brightness_index = 0;
 
-void setup()                    
-{
-//   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-//cs_4_2.set_CS_AutocaL_Millis(0xF);
-   Serial.begin(9600);
-   pinMode(LED, OUTPUT);
-   analogWrite(LED, 0);
+void setup() {
+  //   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  //cs_4_2.set_CS_AutocaL_Millis(0xF);
 
-   avg = cs_4_2.capacitiveSensorRaw(30);
+
+  pinMode(PIN_LED_CONTROL, OUTPUT);
+  analogWrite(PIN_LED_CONTROL, brightness[brightness_index]);
+
+  avg = cs.capacitiveSensorRaw(30);
 }
 
-void loop()                    
-{
-    long start = millis();
-    newValue =  cs_4_2.capacitiveSensorRaw(30);
+void loop() {
+  newValue =  cs.capacitiveSensorRaw(30);
 
-    if (newValue > avg + tolerance) {
-      // change state
-      pressed = true;
-    } else {
-      pressed = false;
-    }
+  if (newValue > avg + TOUCH_TOLERANCE) {
+    // change state
+    pressed = true;
+  } else {
+    pressed = false;
+  }
 
+
+  avg = (avg + newValue) / 2;
+
+  if (pressed && !pressedBuffer) {
+    // Trigger
     
-      avg = (avg + newValue) / 2;
-
-//    Serial.print(millis() - start);        // check on performance in milliseconds
-//    Serial.print("\t");                    // tab character for debug windown spacing
-
-    Serial.println(newValue);                  // print sensor output 1
-
-    if (pressed && !pressedBuffer) {
-      //trigger
-//      flip = !flip;
-      brightness += 85;
-      if (brightness > 255) {
-        brightness = 0;
-      }
-//      digitalWrite(LED, flip);
-      analogWrite(LED, brightness);
+    brightness_index += 1;
+    if (brightness_index == BRIGHTNESS_LEVELS) {
+      brightness_index = 0;
     }
+    
+    analogWrite(PIN_LED_CONTROL, brightness[brightness_index]);
+  }
 
-    pressedBuffer = pressed;
+  pressedBuffer = pressed;
 
-    delay(10);                             // arbitrary delay to limit data to serial port 
+  delay(10);                             // arbitrary delay to limit data to serial port
 }
